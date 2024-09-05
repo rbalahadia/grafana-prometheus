b67c5cddad2f9b6583de7d752090dae5c4eba8a3
unknown <rbalahadia@crescendocollective.com>
2024-09-05 16:18:55 +0800

    additional config

--- Role/grafana-server/setup.sh
+++ Role/grafana-server/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "creating Grafana and prometheus directories"
-mkdir /opt/grafana /opt/prometheus /opt/prometheus/data
-chmod 777 /opt/grafana /opt/prometheus
+mkdir /opt/grafana /opt/prometheus /opt/prometheus/data /opt/grafana/provisioning /opt/grafana/data /opt/grafana/provisioning/dashboards /opt/grafana/provisioning/datasources
+chmod 777 /opt/grafana /opt/prometheus --recursive
 echo "directories created /opt/grafana & /opt/prometheus"
-echo "moving prometheus.yml to /opt/prometheus"
-cp Templates/prometheus.yml /opt/prometheus/
+echo "moving all yaml files"
+cp Templates/Prometheus/prometheus.yml /opt/prometheus/
+cp Templates/Grafana/Dashboard.yml /opt/grafana/provisioning/dashboards/
+cp Templates/Grafana/Datasources.yml /opt/grafana/provisioning/datasources/
 #echo "creating Docker swarm"
 #docker swarm init
 #echo "docker swarm created"
